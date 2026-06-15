8448c1e5dacda5603139f31a6d79bd7a7dc0d465
BlizGroups <blizgroups@gmail.com>
2026-06-15 10:33:28 +0400

    Add multi bitrate conversion

--- ffmpegService.js
+++ ffmpegService.js
@@ -7,13 +7,46 @@ function convertAudio(input, output, bitrate) {
   return new Promise((resolve, reject) => {
     ffmpeg(input)
       .audioBitrate(bitrate)
-      .toFormat("mp3")
+      .audioCodec("libmp3lame")
+      .format("mp3")
       .on("end", () => resolve(output))
       .on("error", reject)
       .save(output);
   });
 }
 
+async function createAllVersions(inputFile) {
+
+  const free64 = "song_64.mp3";
+  const standard128 = "song_128.mp3";
+  const premium320 = "song_320.mp3";
+
+  await convertAudio(
+    inputFile,
+    free64,
+    "64k"
+  );
+
+  await convertAudio(
+    inputFile,
+    standard128,
+    "128k"
+  );
+
+  await convertAudio(
+    inputFile,
+    premium320,
+    "320k"
+  );
+
+  return {
+    free64,
+    standard128,
+    premium320,
+  };
+}
+
 module.exports = {
   convertAudio,
+  createAllVersions,
 };
